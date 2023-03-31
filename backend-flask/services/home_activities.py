76f17e25d7dc61766b8a2f4db6c101bfbbb5b30c
Amelia Hill <amehi4yus@gmail.com>
2023-03-31 03:56:39 +0000

    Add template

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,38 +1,18 @@
 from datetime import datetime, timedelta, timezone
+from opentelemetry import trace
+
 from lib.db import db
 
-# Acquire tracer tobe sent to HoneyComb
-from opentelemetry import trace
-tracer = trace.get_tracer("home.activities")
+#tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  # def run(logger):
-  #   logger.info("HomeActivities")
   def run(cognito_user_id=None):
-    # Create span, name span that will appear in list of stories
-    with tracer.start_as_current_span("home-activities-mock-data"):
-      #Set span attribute
-      span = trace.get_current_span()
-      now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat())
-
-      #sql heredoc
-      results =  db.query_array_json("""
-        SELECT
-          activities.uuid,
-          users.display_name,
-          users.handle,
-          activities.message,
-          activities.replies_count,
-          activities.reposts_count,
-          activities.likes_count,
-          activities.reply_to_activity_uuid,
-          activities.expires_at,
-          activities.created_at
-        FROM public.activities
-        LEFT JOIN public.users ON users.uuid = activities.user_uuid
-        ORDER BY activities.created_at DESC
-      """)
-    
-      return results
+    #logger.info("HomeActivities")
+    #with tracer.start_as_current_span("home-activites-mock-data"):
+    #  span = trace.get_current_span()
+    #  now = datetime.now(timezone.utc).astimezone()
+    #  span.set_attribute("app.now", now.isoformat())
+    sql = db.template('activities','home')
+    results = db.query_array_json(sql)
+    return results
    
